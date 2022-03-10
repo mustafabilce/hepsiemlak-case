8a5b1903ae5980da47e522cc31d4c0b73d58cd10
mustafabilce <52863120+mustafabilce@users.noreply.github.com>
2022-03-10 17:17:33 +0300

    get request from api and show products

---
+++ src/components/product/ProductCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card">
+    <img :src="product.image" alt="" />
+    <h3>{{ product.name }}</h3>
+    <p class="description">{{ product.price }} {{ product.currency }}</p>
+    <button class="add-product-button" @click="addToCart(); $emit('add-basket', product)">Add Basket</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["product"],
+  computed: {
+    description() {
+      return this.product.description.substring(0, 150);
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,47 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <div class="product-card-container">
+      <ProductCard
+        v-for="product in items"
+        :key="product.id"
+        :product="product"
+        v-on:add-basket="viewProduct($event)"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import ProductCard from "../components/product/ProductCard.vue";
 
 export default {
   name: 'Home',
+  components: {
+    ProductCard,
+  },
+  data() {
+    return {
+      items: [],
+      errors: [],
+      product: null,
+      active: {
+        product_notification: false,
+      },
+    };
+  },
+  async created() {
+    await this.getProducts();
+  },
+  methods: {
+    getProducts() {
+      axios
+        .get("https://nonchalant-fang.glitch.me/listing")
+        .then((response) => (this.items = response.data))
+        .catch((error) => {
+          this.errors.push(error);
+        });
+    },
+  }
 }
 </script>
