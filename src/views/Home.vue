50701945bfa8b06851bf17a7efa1ffa2b343841d
mustafabilce <52863120+mustafabilce@users.noreply.github.com>
2022-03-11 12:52:08 +0300

    css adjustment, refactoring, bug-fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="home">
+    <Notification
+      :product="product"
+      :active="active.product_notification"
+      v-on:close-product-notification="closeProductNotification()"
+    />
     <div class="product-card-container">
       <ProductCard
         v-for="product in items"
@@ -14,11 +19,16 @@
 <script>
 import axios from "axios";
 import ProductCard from "../components/product/ProductCard.vue";
+import Notification from "../components/product/Notification.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     ProductCard,
+    Notification,
+  },
+  async created() {
+    await this.getProducts();
   },
   data() {
     return {
@@ -29,9 +39,6 @@ export default {
         product_notification: false,
       },
     };
-  },
-  async created() {
-    await this.getProducts();
   },
   methods: {
     getProducts() {
@@ -46,65 +53,26 @@ export default {
       this.product = product;
       this.active.product_notification = true;
     },
-  }
-}
+    closeProductNotification() {
+      this.active.product_notification = false;
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-.card {
-  width: auto;
-  padding: 10px;
-  border-radius: 5px;
-  background-color: #fff;
-  border: 1px solid rgba(0, 0, 0, 0.125);
-
-  img {
-    max-width: 100px;
-  }
-
-  h5.price {
-    color: gray;
-  }
-
-  p.description {
-    font-size: 0.85rem;
-  }
-
-  p.text-muted {
-    color: gray;
-  }
-}
-
-button.add-basket-button {
-  padding: 10px 20px;
-  background-color: rgb(255, 96, 0);
-  border: none;
-  color: #fff;
-  font-weight: bold;
-  font-size: 0.8rem;
-  border-radius: 0.25rem;
-  cursor: pointer;
-}
-
-button.add-product-button {
-  padding: 10px 20px;
-  background-color: rgb(255, 96, 0);
-  border: none;
-  color: #fff;
-  font-weight: bold;
-  font-size: 0.8rem;
-  border-radius: 0.25rem;
-  cursor: pointer;
-  margin-left: 10px;
-  transition: .3s;
-  &:hover {
-    background-color: rgba(255, 96, 0, .2);
-    color: rgb(255, 96, 0);
-  }
+.product-card-container {
+  width: 100%;
+  margin: 15px 0;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: 1fr;
+  grid-column-gap: 15px;
+  grid-row-gap: 15px;
 }
 @media (max-width: 768px) {
-  .card {
-    margin-bottom: 15px;
+  .product-card-container {
+    display: block;
   }
 }
 </style>
